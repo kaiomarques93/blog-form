4f7a7017659e2191d876581de94883fbee29a408
kaio alexandre marques <alexandremarqueskaio@gmail.com>
2024-12-04 12:54:09 -0300

    update Logo component to change text color from orange to white

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <div className="container mx-auto py-10">
       <main className="container mx-auto">
-      <h1 className="text-3xl font-bold text-center my-8">Minha Coleção de Cards</h1>
+      <h1 className="text-3xl font-bold text-center my-8">Minha Coleção de Blogs</h1>
       <CardGrid items={blogs} />
     </main>
     </div>
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href='/'>
-        <div className="bg-blue-400 text-orange-400 font-bold rounded-md p-3">Cubo</div>
+        <div className="bg-blue-400 text-white font-bold rounded-md p-3">Cubo</div>
     </Link>
   )
 }
