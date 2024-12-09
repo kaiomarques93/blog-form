74e8193162ea31fd1e24854c5d803da2093d3c82
kaio alexandre marques <alexandremarqueskaio@gmail.com>
2024-12-09 15:07:19 -0300

    refactor CardGrid component to simplify Card rendering

--- src/components/card-grid.tsx
+++ src/components/card-grid.tsx
@@ -25,10 +25,7 @@ export function CardGrid({ items }: CardGridProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
       {items.map((item) => (
-        <Card
-          key={item.id}
-          className="flex flex-col" 
-        >
+        <Card key={item.id} className="flex flex-col">
           <CardHeader>
             {item.image && (
               <img
